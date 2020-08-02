76181dfad1b79252b26e879a5e9d37bf02e11c23
Abdulsalam <abdulsalamabodunrin369@gmail.com>
2020-08-02 19:40:42 +0100

    keeper app

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import DeleteForever  from "@material-ui/icons/Delete";
+function Header() {
+  return (
+    <header>
+      <h1>
+        {/* <DeleteForever /> */}
+        Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
